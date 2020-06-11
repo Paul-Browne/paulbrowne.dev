13ffe93b6f9765583b0b94393bdaa2d4a4bef26f
paul browne <paulmatthewbrowne@gmail.com>
2020-06-11 16:34:18 +0300

    adds like my post

--- app.js
+++ app.js
@@ -1,5 +1,63 @@
+const puppeteer = require("puppeteer");
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.send('Hello World'));
+
+// serve public from root
+app.use('/', express.static('public'));
+
+const credentials = [
+  {
+    username: "paulmatthewbrowne@gmail.com",
+    password: ":$q7*UfRUQ$i*Hj"
+  }
+];
+
+async function puppet(user, url, res){
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+  })
+  const page = await browser.newPage();
+  await page.goto(`https://www.linkedin.com/uas/login?session_redirect=${url}`);
+  await page.waitFor(3534);
+  await page.focus('#username')
+  await page.keyboard.type(user.username);
+  await page.waitFor(394);
+  await page.focus('#password')
+  await page.keyboard.type(user.password);
+  await page.waitFor(513);
+  await page.click('button[type="submit"]');
+  await page.waitForNavigation();
+  if (await page.$('button[aria-pressed="false"][aria-label^="Like"][aria-label$="post"]') !== null){
+    await page.click('button[aria-pressed="false"][aria-label^="Like"][aria-label$="post"]');
+    await page.waitFor(1033);
+    res.write(`<br/><br/><a href="${url}">post liked<a>`);
+    res.end();
+    await browser.close();
+  }else{
+    res.write(`<br/><br/><a href="${url}">post was already liked<a>`);
+    res.end();
+    await browser.close();
+  }
+  //await page.click('.share-reshare-button button');
+  //await page.waitFor(853);
+  //await page.click('button[data-control-name="share.post"]');
+  //await page.waitFor(250000);
+};
+
+
+
+app.use('/like-my-post', function(req, res){
+  if(req.query.url){
+    res.write(`<a target="_blank" href="${req.query.url}">liking this post... wait a minute<a>`);
+    credentials.forEach( function(user, index) {
+      puppet(user, req.query.url, res);
+    });   
+  }else{
+    res.send('no url');
+  }
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
