395ff1c7a73d35fc13256deecf34c3be2c143660
paul browne <paulmatthewbrowne@gmail.com>
2020-06-11 17:33:31 +0300

    adds args

--- app.js
+++ app.js
@@ -13,49 +13,45 @@ const credentials = [
   }
 ];
 
-async function puppet(user, url, res){
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox']
-  });
-  const page = await browser.newPage();
-  await page.goto(`https://www.linkedin.com/uas/login?session_redirect=${url}`);
-  await page.waitFor(3534);
-  res.write(`<p>typing username</p>`);
-  await page.focus('#username');
-  await page.keyboard.type(user.username);
-  await page.waitFor(394);
-  res.write(`<p>typing password</p>`);
-  await page.focus('#password')
-  await page.keyboard.type(user.password);
-  await page.waitFor(513);
-  await page.click('button[type="submit"]');
-  await page.waitForNavigation();
-  const loc2 = await page.evaluate(() => {return location.href });
-  res.write(`<p>${loc2}</p>`);
-  if (await page.$('button[aria-pressed="false"][aria-label^="Like"][aria-label$="post"]') !== null){
-    await page.click('button[aria-pressed="false"][aria-label^="Like"][aria-label$="post"]');
-    await page.waitFor(1033);
-    res.write(`<br/><br/><a href="${url}">post liked<a>`);
-    res.end();
-    await browser.close();
-  }else{
-    res.write(`<br/><br/><a href="${url}">post was already liked<a>`);
-    res.end();
-    await browser.close();
-  }
-  //await page.click('.share-reshare-button button');
-  //await page.waitFor(853);
-  //await page.click('button[data-control-name="share.post"]');
-  //await page.waitFor(250000);
-};
-
-
-
 app.use('/like-my-post', function(req, res){
   if(req.query.url){
     res.write(`<a target="_blank" href="${req.query.url}">liking this post...<a><br><span>wait a minute</span>`);
     credentials.forEach( function(user, index) {
-      puppet(user, req.query.url, res);
+      (async () => {
+        const browser = await puppeteer.launch({
+          args: ['--no-sandbox']
+        });
+        const page = await browser.newPage();
+        await page.goto(`https://www.linkedin.com/uas/login?session_redirect=${req.query.url}`);
+        await page.waitFor(3534);
+        res.write(`<p>typing username</p>`);
+        await page.focus('#username');
+        await page.keyboard.type(user.username);
+        await page.waitFor(394);
+        res.write(`<p>typing password</p>`);
+        await page.focus('#password')
+        await page.keyboard.type(user.password);
+        await page.waitFor(513);
+        await page.click('button[type="submit"]');
+        await page.waitForNavigation();
+        const loc2 = await page.evaluate(() => {return location.href });
+        res.write(`<p>${loc2}</p>`);
+        if (await page.$('button[aria-pressed="false"][aria-label^="Like"][aria-label$="post"]') !== null){
+          await page.click('button[aria-pressed="false"][aria-label^="Like"][aria-label$="post"]');
+          await page.waitFor(1033);
+          res.write(`<br/><br/><a href="${req.query.url}">post liked<a>`);
+          res.end();
+          await browser.close();
+        }else{
+          res.write(`<br/><br/><a href="${req.query.url}">post was already liked<a>`);
+          res.end();
+          await browser.close();
+        }
+        //await page.click('.share-reshare-button button');
+        //await page.waitFor(853);
+        //await page.click('button[data-control-name="share.post"]');
+        //await page.waitFor(250000);
+      })();
     });   
   }else{
     res.send('no url');
